a04bcdaae8da1d8bf86d20297bbc59eef20b7fda
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2025-09-09 23:15:21 +0530

    Time: 24 ms (40.12%), Space: 55.7 MB (40.59%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -2,36 +2,32 @@ class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         List<List<Integer>> reverseAdj= new ArrayList<>();
         int V= graph.length;
-        int indegree[]= new int[V];
+        int outDegree[]= new int[V];
         for(int i=0; i<V; i++){
             reverseAdj.add(new ArrayList<>());
         }
         for(int i=0; i<V; i++){
             for(int a: graph[i]){
                 reverseAdj.get(a).add(i);
-                indegree[i]++;
+                outDegree[i]++;
             }
         }
         Queue<Integer> queue= new LinkedList<>();
-        Stack<Integer> stack= new Stack<>();
         for(int i=0; i<V; i++){
-            if(indegree[i]==0){
+            if(outDegree[i]==0){
                 queue.offer(i);
             }
         }
+        List<Integer> eSafe= new ArrayList<>();
         while(!queue.isEmpty()){
             int cur= queue.poll();
-            stack.push(cur);
+            eSafe.add(cur);
             for(int a: reverseAdj.get(cur)){
-                indegree[a]--;
-                if(indegree[a]==0){
+                outDegree[a]--;
+                if(outDegree[a]==0){
                     queue.offer(a);
                 }            
             }
-        }
-        List<Integer> eSafe= new ArrayList<>();
-        while(!stack.isEmpty()){
-            eSafe.add(stack.pop());
         }
         Collections.sort(eSafe);
         return eSafe;
