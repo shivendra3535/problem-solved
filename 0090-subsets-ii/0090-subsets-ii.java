ebb7602dc427bb1942f443d9f0f83dddaa0f88dc
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-02-04 23:37:41 +0530

    Time: 3 ms (54.37%), Space: 45.3 MB (38.46%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,20 +1,20 @@
 class Solution {
-    public void subsets(int index,HashSet<List<Integer>> res,List<Integer> ds,int[] nums){
-        if(index>=nums.length){
-            res.add(new ArrayList<>(ds));
-            return;
+    public void subsetsWithDup(int[] nums,int index,List<List<Integer>> res, List<Integer> ds){
+        
+        res.add(new ArrayList<>(ds));
+        
+        for(int i=index; i<nums.length; i++){
+            if(i>index && nums[i]==nums[i-1]) continue;
+            ds.add(nums[i]);
+            subsetsWithDup(nums,i+1,res,ds);
+            ds.remove(ds.size()-1);
         }
-        ds.add(nums[index]);
-        subsets(index+1, res, ds, nums);
-
-        ds.remove(ds.size()-1);
-        subsets(index+1, res, ds, nums);
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        HashSet<List<Integer>> res= new HashSet<>();
-        List<Integer> ds= new ArrayList<Integer>();
-        subsets(0,res,ds,nums);
-        return (new ArrayList<>(res));
+        List<List<Integer>> res= new ArrayList<>();
+        List<Integer> ds= new ArrayList<>();
+        subsetsWithDup(nums,0,res,ds);
+        return res;
     }
 }
