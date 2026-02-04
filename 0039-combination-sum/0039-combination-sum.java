a1c7ad73bf857bfeb9e93598eea3731eb0ab4766
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-02-04 22:28:21 +0530

    Time: 2 ms (95.2%), Space: 45.7 MB (65.36%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,46 +1,22 @@
-import java.util.*;
-
 class Solution {
-
-    public List<List<Integer>> combinationSum(int index, List<Integer> ans, List<List<Integer>> res, int[] candidates, int target) {
-        if (index == candidates.length) {
-            if (target == 0) {
-                res.add(new ArrayList<>(ans)); // Copy current combination
-            }
-            return res;
+    public void combinationSum(int []candidates, int index, List<Integer> ds, int target, List<List<Integer>> res){
+        if(target==0){
+            res.add(new ArrayList<>(ds));
+            return;
+        }
+        if(index==candidates.length || target<0){
+            return;
         }
 
-        if (candidates[index] <= target) {
-            ans.add(candidates[index]);
-            combinationSum(index, ans, res, candidates, target - candidates[index]);
-            ans.remove(ans.size() - 1);
-        }
-        combinationSum(index + 1, ans, res, candidates, target);
-
-        return res;
+        ds.add(candidates[index]);
+        combinationSum(candidates,index,ds,target-candidates[index],res);
+        ds.remove(ds.size()-1);
+        combinationSum(candidates,index+1,ds,target,res);
     }
-    
-    // Wrapper function
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
-        return combinationSum(0, ans, res, candidates, target);
-        
-    }
-
-    public static void main(String args[]) {
-        Solution obj = new Solution();
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int target = sc.nextInt();
-        int candidates[] = new int[n];
-        
-        for (int i = 0; i < n; i++) {
-            candidates[i] = sc.nextInt();
-        }
-        List<List<Integer>> result = obj.combinationSum(candidates, target);
-
-        System.out.println(result);
-        sc.close();
+        List<List<Integer>> res= new ArrayList<>();
+        List<Integer> ds= new ArrayList<>();
+        combinationSum(candidates,0,ds,target,res);
+        return res;
     }
 }
