df256080fefc0118228e306d387333ec40bb9fb7
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-02-05 22:37:55 +0530

    Time: 8 ms (74.1%), Space: 65.4 MB (36.62%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,29 +1,31 @@
 class Solution {
-    public List<List<String>> partition(String s) {
-        List<List<String>> res=new ArrayList<>();
-        List<String> ans= new ArrayList<>();
-        partition(s,0,ans,res);
-        return res;
-    }
     public boolean isPalindrome(String s, int start, int end){
         while(start<=end){
-            if(s.charAt(start++)!=s.charAt(end--)){
+            if(s.charAt(start)!=s.charAt(end)){
                 return false;
             }
+            start++;
+            end--;
         }
         return true;
     }
-    public void partition(String s,int index,List<String> ans, List<List<String>> res){
-        if(index==s.length()){
-            res.add(new ArrayList<>(ans));
-            return;
+    public void partition(String s, int index, List<List<String>>res, List<String> ds){
+        if(index>=s.length()){
+            res.add(new ArrayList<>(ds));
         }
+
         for(int i=index; i<s.length(); i++){
-            if(isPalindrome(s, index, i)){
-                ans.add(s.substring(index,i+1));
-                partition(s,i+1,ans,res);
-                ans.remove(ans.size()-1);
+            if(isPalindrome(s,index,i)){
+                ds.add(s.substring(index,i+1));
+                partition(s,i+1,res,ds);
+                ds.remove(ds.size()-1);
             }
         }
-    } 
+    }
+    public List<List<String>> partition(String s) {
+        List<List<String>>res= new ArrayList<>();
+        List<String> ds= new ArrayList<>();
+        partition(s,0,res,ds);
+        return res;
+    }
 }
