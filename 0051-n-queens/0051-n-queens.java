7ecb34b27bee73065211bb22b86dc6e710952ef0
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-02-07 00:46:19 +0530

    Time: 3 ms (45.67%), Space: 46.6 MB (79.57%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,49 +1,44 @@
-import java.util.*;
-
 class Solution {
-
-    public List<List<String>> solveNQueens(int n) {
-        List<List<String>> result = new ArrayList<>();
-        char[][] board = new char[n][n];
-
-        // Initialize the board with '.'
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(board[i], '.');
+    public boolean isSafe(char [][]board, int row, int col){
+        for(int i=0; i<board.length; i++){
+            if(board[row][i]=='Q') return false;
         }
-        boolean cols[]=new boolean[n];
-        boolean leftDiagonal[]= new boolean[2*n-1];
-        boolean rightDiagonal[]= new boolean[2*n-1];
-
-        // Start backtracking
-        backtrack(result, board, 0, n, cols, leftDiagonal, rightDiagonal);
-        return result;
+        for(int i=0; i<board.length; i++){
+            if(board[i][col]=='Q') return false;
+        }
+        for(int i=row,j=col; i>=0 && j>=0; i--,j--){
+            if(board[i][j]=='Q') return false;
+        }
+        for(int i=row,j=col; i>=0 && j<board.length; i--,j++){
+            if(board[i][j]=='Q') return false;
+        }
+        return true;
     }
-
-    private void backtrack(List<List<String>> result, char[][] board, int row, int n, boolean cols[], boolean leftDiagonal[], boolean rightDiagonal[]){
-        if (row == n) {
-            List<String> temp = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
+    public void solveNQueens(char [][]board, int row, int n, List<List<String>> res){
+        if(row==n){
+            List<String> temp= new ArrayList<>();
+            for(int i=0; i<n; i++){
                 temp.add(new String(board[i]));
             }
-            result.add(temp);
+            res.add(temp);
             return;
         }
 
-        for (int j = 0; j < n; j++) {
-            int leftIndex= row+j;
-            int rightIndex= n-1+row-j;
-            if (!cols[j] && !leftDiagonal[leftIndex] && !rightDiagonal[rightIndex]){
-                board[row][j] = 'Q';
-                cols[j]=true;
-                leftDiagonal[leftIndex]=true;
-                rightDiagonal[rightIndex]=true;
-                backtrack(result, board, row + 1, n, cols, leftDiagonal, rightDiagonal);
-                board[row][j] = '.';
-                cols[j]=false;
-                leftDiagonal[leftIndex]=false;
-                rightDiagonal[rightIndex]=false;
+        for(int j=0; j<n; j++){
+            if(isSafe(board,row,j)){
+                board[row][j]='Q';
+                solveNQueens(board,row+1,n,res);
+                board[row][j]='.';
             }
         }
     }
-
+    public List<List<String>> solveNQueens(int n) {
+        char board[][]= new char[n][n];
+        for(int i=0; i<n; i++){
+            Arrays.fill(board[i],'.');
+        }
+        List<List<String>> res= new ArrayList<>();
+        solveNQueens(board,0,n,res);
+        return res;
+    }
 }
