05cc461a52d31fadd66cd33b75fe0220daf9661c
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-02-07 02:21:22 +0530

    Time: 80 ms (9.92%), Space: 58.9 MB (96.85%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,74 +1,59 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
-class Solution {
-
-    public static void merge(int[] arr, int low, int mid, int high) {
-        ArrayList<Integer> temp = new ArrayList<>();
-        int left = low;
-        int right = mid + 1;
-        while (left <= mid && right <= high) {
-            if (arr[left] <= arr[right]) {
+public class Solution {
+    static void merge(int arr[],int low,int mid, int high){
+        int left=low;
+        int right=mid+1;
+        ArrayList<Integer> temp= new ArrayList<>();
+        while(left<=mid && right<=high){
+            if(arr[left]<=arr[right]){
                 temp.add(arr[left]);
                 left++;
-            } else {
+            }
+            else{
                 temp.add(arr[right]);
                 right++;
             }
         }
-        while (left <= mid) {
+        while(left<=mid){
             temp.add(arr[left]);
             left++;
         }
-        while (right <= high) {
+        while(right<=high){
             temp.add(arr[right]);
             right++;
         }
-        for (int i = low; i <= high; i++) {
-            arr[i] = temp.get(i - low);
+        for(int i=low; i<=high; i++){
+            arr[i]=temp.get(i-low);
         }
     }
-
-    public static int reversePairs(int[] nums, int low, int mid, int high) {
-        int right = mid + 1;
-        int count = 0;
-        for (int left = low; left <= mid; left++) {
-            while (right <= high && (long) nums[left] > 2L * nums[right]) {
+    static int countReverse(int nums[], int low, int high, int mid){
+        int right=mid+1;
+        int cnt=0;
+        for(int left=low; left<=mid; left++){
+            while(right<=high && (long)nums[left]>2L*nums[right]){
                 right++;
             }
-            count += (right - (mid + 1));
+            cnt+=right-(mid+1);
         }
-        return count;
-    }
-
-    public static int mergeSort(int[] nums, int low, int high) {
-        int cnt = 0;
-        if (low >= high) {
-            return cnt;
-        }
-        int mid = (low + high) / 2;
-        cnt += mergeSort(nums, low, mid);
-        cnt += mergeSort(nums, mid + 1, high);
-        cnt += reversePairs(nums, low, mid, high);
-        merge(nums, low, mid, high);
         return cnt;
     }
-
-    // Wrapper function to match the driver code expectation
-    public int reversePairs(int[] nums) {
-        return mergeSort(nums, 0, nums.length - 1);
+    static int mergeSort(int arr[], int low, int high){
+    if(low >= high){
+        return 0;
     }
+    int mid = (low + high) / 2;
+    int cnt = 0;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] nums = new int[n];
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
-        }
-        Solution solution = new Solution();
-        System.out.println(solution.reversePairs(nums));
-        sc.close();
-    }
+    cnt += mergeSort(arr, low, mid);
+    cnt += mergeSort(arr, mid+1, high);
+
+    // ✅ FIXED ORDER HERE
+    cnt += countReverse(arr, low, high, mid);
+
+    merge(arr, low, mid, high);
+    return cnt;
 }
 
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums,0,nums.length-1);
+    }
+}
