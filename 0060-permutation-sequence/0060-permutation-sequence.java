3921be04d929a51dbc9229c78b31e907c8f85da0
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-02-06 23:38:58 +0530

    Time: 2 ms (42.21%), Space: 43.1 MB (23.55%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -3,18 +3,18 @@ class Solution {
         int fact=1;
         List<Integer> res= new ArrayList<>();
         for(int i=1; i<n; i++){
-            fact = fact*i;
+            fact*=i;
             res.add(i);
         }
+        res.add(n);
         String ans="";
         k=k-1;
-        res.add(n);
         while(true){
-            ans = ans+res.get(k / fact);
-            res.remove(k / fact);
-            if(res.size() == 0)break;
-            k = k % fact;
-            fact = fact/res.size();
+            ans+=res.get(k/fact);
+            res.remove(k/fact);
+            if(res.size()==0) break;
+            k=k%fact;
+            fact=fact/res.size();
         }
         return ans;
     }
