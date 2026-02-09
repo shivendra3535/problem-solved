7cb3c482560fc582e5dd9a4955548129fa82508b
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-02-09 15:20:07 +0530

    Time: 384 ms (12.58%), Space: 43.1 MB (14.79%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,32 +1,31 @@
 class Solution {
-    public static boolean isValid(char[][] board, int row, int col, char c) {
-        for (int i = 0; i < board.length; i++) {
-            if (board[row][i] == c) return false;
-            if (board[i][col] == c) return false;
-            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+    public boolean validNum(char [][]board, int row, int col,char c){
+        for(int i=0; i<board.length; i++){
+            if(board[row][i]==c) return false;
+            if(board[i][col]==c) return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c) return false;
         }
         return true;
     }
-
-    public static boolean solveSudoku2(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                if (board[i][j] == '.') {
-                    for (char c = '1'; c <= '9'; c++) {
-                        if (isValid(board, i, j, c)) {
-                            board[i][j] = c;
-                            if (solveSudoku2(board)) return true;
-                            board[i][j] = '.'; // backtrack
+    public boolean solver(char[][] board) {
+        for(int row=0; row<board.length; row++){
+            for(int col=0; col<board.length; col++){
+                if(board[row][col]=='.'){
+                    for(char c='1'; c<='9'; c++){
+                        if(validNum(board,row,col,c)){
+                            board[row][col]=c;
+                            if(solver(board)) return true;
+                            board[row][col]='.';
                         }
                     }
-                    return false; // no valid number found
+                    return false;
                 }
             }
         }
-        return true; // all cells filled correctly
+        return true;
+    }
+    public void solveSudoku(char[][] board){
+        boolean b=solver(board);
     }
 
-    public void solveSudoku(char[][] board) {
-        solveSudoku2(board);
-    }
 }
