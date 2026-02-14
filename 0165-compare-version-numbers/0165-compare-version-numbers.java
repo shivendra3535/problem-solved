74efa7d0b68070155ca75ff5d821d7582264e045
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-02-14 21:51:36 +0530

    Time: 0 ms (100%), Space: 42.6 MB (97.54%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        int n=version1.length();
+        int m=version2.length();
+        int i=0;
+        int j=0;
+        while(i<n || j<m){
+            int nums1=0;
+            int nums2=0;
+            while(i<n && version1.charAt(i)!='.'){
+                nums1=nums1*10+version1.charAt(i)-'0';
+                i++;
+            }
+            while(j<m && version2.charAt(j)!='.'){
+                nums2=nums2*10+ version2.charAt(j)-'0';
+                j++;
+            }
+
+            if(nums1<nums2) return -1;
+            if(nums1>nums2) return 1;
+
+            if (i < n) i++;
+            if (j < m) j++;
+        }
+        return 0;
+    }
+}
