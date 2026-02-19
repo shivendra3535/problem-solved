cf43fc3b898db1738a0e781f8efa62ff17b49a6c
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-02-19 23:02:05 +0530

    Time: 1 ms (60.12%), Space: 47.4 MB (70.79%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,33 +1,48 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-         ArrayList<Integer>rows_0= new ArrayList<Integer>();
-        ArrayList<Integer>cols_0= new ArrayList<Integer>();
         int m=matrix.length;
         int n=matrix[0].length;
-        for(int i=0;i<m; i++)
-        {
-            for(int j=0;j<n; j++)
-            {
-                if(matrix[i][j]==0)
-                {
-                    rows_0.add(i);
-                    cols_0.add(j);
+        boolean checkRow=false;
+        boolean checkCol=false;
+        for(int i=0; i<n; i++){
+            if(matrix[0][i]==0) {
+                checkRow=true;
+                break;
+            }
+        }
+        for(int i=0; i<m; i++){
+            if(matrix[i][0]==0) {
+                checkCol=true;
+                break;
+            }
+        }
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
                 }
             }
         }
-        for(int i=0; i<rows_0.size();i++)
-        {
-            int a=rows_0.get(i);
-            int b=cols_0.get(i);
-            for(int k=0; k<n;k++)
-            {
-                matrix[a][k]=0;
-            }
-             for(int l=0; l<m;l++)
-            {
-                matrix[l][b]=0;
+
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
+                    matrix[i][j]=0;
+                }
             }
         }
-
+       
+        if(checkRow){
+            for(int i=0; i<n; i++){
+                 matrix[0][i]=0;
+            }
+        }
+        
+        if(checkCol){
+            for(int i=0; i<m; i++){
+                matrix[i][0]=0;
+            }
+        }
     }
 }
