86919fecfbfc84339f6c12c62dbe19db2206c162
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-02-20 13:02:51 +0530

    Time: 7 ms (76.61%), Space: 50.1 MB (31.76%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -6,13 +6,11 @@ class Solution {
             char arr[]= s.toCharArray();
             Arrays.sort(arr);
             String sorted= new String(arr);
-            if(!map.containsKey(sorted)){
-                map.put(sorted, new ArrayList<>());
-            }
+            if(!map.containsKey(sorted)) map.put(sorted, new ArrayList<>());
             map.get(sorted).add(s);
         }
-        for(List<String> ls: map.values()){
-            res.add(ls);
+        for(List<String> s: map.values()){
+            res.add(s);
         }
         return res;
     }
