d3555f9a51e21eabdad73603e470fd04d933846c
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-02-23 15:24:17 +0530

    Time: 99 ms (15.9%), Space: 47.4 MB (45.47%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,31 +1,34 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s.length()<t.length()) return "";
+        int n=s.length();
+        int m=t.length();
+        if(n<m) return "";
         String min="";
         int minLen=Integer.MAX_VALUE;
+        int right=0;
+        int left=0;
         int freq1[]= new int[128];
         int freq2[]= new int[128];
         for(char c: t.toCharArray()){
             freq1[c]++;
         }
-        int curMatch=0;
         int required=0;
+        int curMatch=0;
         for(int i=0; i<128; i++){
             if(freq1[i]>0) required++;
         }
-        int left=0;
-        int right=0;
-        while(right<s.length()){
+        while(right<n){
             freq2[s.charAt(right)]++;
             if(freq2[s.charAt(right)]==freq1[s.charAt(right)]) curMatch++;
             while(left<=right && curMatch==required){
-                if(minLen>right-left+1){
+                int len=right-left+1;
+                if(len<minLen){
                     minLen=right-left+1;
                     min=s.substring(left,right+1);
                 }
-                if(freq2[s.charAt(left)]==freq1[s.charAt(left)]) curMatch--;
+                if(freq1[s.charAt(left)]==freq2[s.charAt(left)]) curMatch--;
                 freq2[s.charAt(left)]--;
-                left++;           
+                left++;
             }
             right++;
         }
