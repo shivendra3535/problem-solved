6ce6667e836890b0afea9954018185af766a1c28
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-02-28 11:42:17 +0530

    Time: 0 ms (100%), Space: 43.6 MB (96.29%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,31 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
         int low=0;
-        int high= nums.length-1;
+        int high=nums.length-1;
         while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target)return mid;
-            //checking left side if it is sorted or not
+            int mid=(low+high)>>1;
+            if(nums[mid]==target){
+                return mid;
+            }
             if(nums[low]<=nums[mid]){
-                //if left side is sorted, we will check if solution lies in index range low to mid
-                if(target >= nums[low] && target<=nums[mid]){
-                    //means solution will exist in left side only, no need to check right side
+                if(nums[low]<=target && target<nums[mid]){
                     high=mid-1;
                 }
                 else{
-                    //if solution not exist in sorted left part , we will move to right side 
-                    low= mid+1;
+                    low=mid+1;
                 }
             }
             else{
-                //left side is not sorted, means right side is sorted.Check if target lies in index range mid to high
-                if(target >= nums[mid] && target <=nums[high]){
-                    //means  solution exist in right side ,no need to check left side
+                if(nums[mid+1]<=target && nums[high]>=target){
                     low=mid+1;
                 }
                 else{
-                    //if solution is not at right side move to left part
-                    high= mid-1;
+                    high=mid-1;
                 }
             }
         }
