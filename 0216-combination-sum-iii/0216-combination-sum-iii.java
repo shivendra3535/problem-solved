6ea5d133bb11568d7ea8ee3ee14033752c7d8c60
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-03-25 22:43:38 +0530

    Time: 0 ms (100%), Space: 42.6 MB (40.76%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,24 +1,23 @@
 class Solution {
-    public void combinationSum3(int k, int n, int index,int sum, int cnt,List<Integer> ds, List<List<Integer>> res){
+    public void combinationSum3(int k, int n, int index,int cnt, List<Integer> ds, List<List<Integer>> res, int sum) {
         if(cnt==k){
             if(sum==n){
                 res.add(new ArrayList<>(ds));
             }
             return;
         }
-        if(index>=9)
-        {
-            return;
-        }
-            ds.add(index+1);
-            combinationSum3(k,n,index+1,sum+index+1,cnt+1,ds,res);
-            ds.remove(ds.size()-1);
-        combinationSum3(k,n,index+1,sum,cnt,ds,res);
+        
+        if(index>=9) return;
+        ds.add(index+1);
+        combinationSum3(k,n,index+1,cnt+1,ds,res,sum+index+1);
+        ds.remove(ds.size()-1);
+        combinationSum3(k,n,index+1,cnt,ds,res,sum);
+        
     }
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> res= new ArrayList<>();
         List<Integer> ds= new ArrayList<>();
-        combinationSum3(k,n,0,0,0,ds,res);
+        List<List<Integer>> res= new ArrayList<>();
+        combinationSum3(k,n,0,0,ds,res,0);
         return res;
     }
 }
