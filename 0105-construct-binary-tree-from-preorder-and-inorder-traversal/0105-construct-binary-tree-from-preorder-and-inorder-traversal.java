e0d95517bef70c664601f908fb62da1a51287168
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-04-07 16:24:58 +0530

    Time: 2 ms (63.94%), Space: 46.6 MB (19.12%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,23 +15,20 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        Map<Integer, Integer> inMap= new HashMap<>();
+        HashMap<Integer,Integer> inMap= new HashMap<>();
         for(int i=0; i<inorder.length; i++){
             inMap.put(inorder[i],i);
         }
-        TreeNode root= buildTree1(preorder,0,preorder.length-1,inorder,0,inorder.length-1,inMap);
+        TreeNode root=buildTree(preorder,0,preorder.length-1,inorder,0,inorder.length-1,inMap);
         return root;
     }
-
-    public TreeNode buildTree1(int []preorder,int preStart, int preEnd, int[]inorder, int inStart, int inEnd, Map<Integer, Integer> inMap){
-        if(preStart > preEnd || inStart > inEnd){
-            return null;
-        }
+    public TreeNode buildTree(int []preorder, int preStart, int preEnd, int[]inorder, int inStart, int inEnd, HashMap<Integer,Integer> inMap){
+        if(preStart>preEnd || inStart>inEnd) return null;
         TreeNode root= new TreeNode(preorder[preStart]);
-        int inRoot= inMap.get(root.val);
-        int numLeft= inRoot-inStart;
-        root.left= buildTree1(preorder,preStart+1,preStart+numLeft,inorder,inStart,inRoot-1,inMap);
-        root.right= buildTree1(preorder, preStart+numLeft+1, preEnd, inorder, inRoot+1, inEnd, inMap);
+        int inRoot=inMap.get(preorder[preStart]);
+        int numLeft=inRoot-inStart;
+        root.left=buildTree(preorder,preStart+1,preStart+numLeft,inorder,inStart,inRoot-1,inMap);
+        root.right=buildTree(preorder,preStart+numLeft+1,preEnd,inorder,inRoot+1,inEnd,inMap);
         return root;
     }
 }
