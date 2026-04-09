da3f5d5122bab78ab59868a4b766485db202f6ae
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-04-09 19:12:51 +0530

    Time: 0 ms (100%), Space: 47.7 MB (24.03%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -14,52 +14,40 @@
  * }
  */
 class Solution {
-    public TreeNode findLastRight(TreeNode root){
-        if(root.right==null){
-            return root;
-        }
-        return findLastRight(root.right);
+    public TreeNode lastRight(TreeNode root){
+        if(root.right==null) return root;
+        return lastRight(root.right);
     }
     public TreeNode helper(TreeNode root){
-        if(root.left==null){
-            return root.right;
-        }
-        else if(root.right==null){
-            return root.left;
-        }
-        else{
-            TreeNode rightChild=root.right;
-            TreeNode lastRight=findLastRight(root.left);
-            lastRight.right=rightChild;
-            return root.left;
-        }
+        if(root.left==null) return root.right;
+        if(root.right==null) return root.left;
+        TreeNode rightChild=root.right;
+        TreeNode leftRight=lastRight(root.left);
+        leftRight.right=rightChild;
+        return root.left;
     }
     public TreeNode deleteNode(TreeNode root, int key) {
         if(root==null) return null;
-        if(root.val==key){
-            return helper(root);
-        }
-        TreeNode dummy= root;
+        if(root.val==key) return helper(root);
+
+        TreeNode dummy=root;
         while(root!=null){
-            if(root.val > key){
-                if(root.left!=null && root.left.val==key){
-                    root.left= helper(root.left);
+            if(root.val<key){
+                if(root.right!=null && root.right.val==key){
+                    root.right=helper(root.right);
                     break;
                 }
-                else{
-                    root=root.left;
-                }
+                root=root.right;
             }
             else{
-                if(root.right!=null && root.right.val==key){
-                    root.right= helper(root.right);
+                if(root.left!=null && root.left.val==key){
+                    root.left=helper(root.left);
                     break;
                 }
-                else{
-                    root=root.right;
-                }
-                }
+                root=root.left;
+            }
         }
         return dummy;
     }
 }
+            
