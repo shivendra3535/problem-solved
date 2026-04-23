6bf1510ea45a8990e84dc5b2014d7157ade4ede6
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-04-23 23:56:46 +0530

    Time: 2 ms (11.32%), Space: 47.1 MB (12.74%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,55 +1,41 @@
-class Pair{
-    int row;
-    int col;
-    Pair(int row,int col){
-        this.row=row;
-        this.col=col;
-    }
-}
 class Solution {
-    public void bfs(int[][] image,int sr,int sc,int color,int prev,boolean vis[][]){
-        int m=image.length;
-        int n=image[0].length;
-        Queue<Pair> queue= new LinkedList<>();
-        queue.offer(new Pair(sr,sc));
-        vis[sr][sc]=true;
-        image[sr][sc]=color;
-        while(!queue.isEmpty()){
-            Pair p= queue.poll();
-            int r=p.row;
-            int c=p.col;
-
-            if(c>0 && image[r][c-1]==prev && !vis[r][c-1]){
-                vis[r][c-1]=true;
-                image[r][c-1]=color;
-                queue.offer(new Pair(r,c-1));
-            }
-
-            if(c<n-1 && image[r][c+1]==prev && !vis[r][c+1]){
-                vis[r][c+1]=true;
-                image[r][c+1]=color;
-                queue.offer(new Pair(r,c+1));
-            }
-
-            if(r>0 && image[r-1][c]==prev && !vis[r-1][c]){
-                vis[r-1][c]=true;
-                image[r-1][c]=color;
-                queue.offer(new Pair(r-1,c));
-            }
-            
-            if(r<m-1 && image[r+1][c]==prev && !vis[r+1][c]){
-                vis[r+1][c]=true;
-                image[r+1][c]=color;
-                queue.offer(new Pair(r+1,c));
-            }
+    class Pair{
+        int row;
+        int col;
+        Pair(int row, int col){
+            this.row=row;
+            this.col=col;
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m=image.length;
-        int n=image[0].length;
-        boolean vis[][]=new boolean[m][n];
-        int prev=image[sr][sc];
-        bfs(image,sr,sc,color,prev,vis);
+        int n=image.length;
+        int m=image[0].length;
+        boolean vis[][] = new boolean[n][m];
+        vis[sr][sc]=true;
+        int original=image[sr][sc];
+        image[sr][sc]=color;
+        Queue<Pair> q= new LinkedList<>();
+        q.offer(new Pair(sr,sc));
+        int dir[][]={
+            {-1,0},
+            {1,0},
+            {0,-1},
+            {0,1}
+        };
+        while(!q.isEmpty()){
+            Pair node=q.poll();
+            int r=node.row;
+            int c=node.col;
+            for(int d[]: dir){
+                int nR=r+d[0];
+                int nC=c+d[1];
+                if(nR>=0 && nC>=0 && nR<n && nC<m && image[nR][nC]==original && !vis[nR][nC]){
+                    vis[nR][nC]=true;
+                    image[nR][nC]=color;
+                    q.offer(new Pair(nR,nC));
+                }
+            }
+        }
         return image;
     }
 }
