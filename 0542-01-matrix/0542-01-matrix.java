c63030a6618a2ddbe27bcf5ba7bb06829b707d5e
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-04-25 23:01:18 +0530

    Time: 15 ms (42.06%), Space: 49 MB (56.72%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,45 +1,42 @@
-class Node{
-    int row;
-    int col;
-    int step;
-    Node(int row,int col,int step){
-        this.row=row;
-        this.col=col;
-        this.step=step;
-    }
-}
 class Solution {
+    class Pair{
+        int row;
+        int col;
+        int step;
+        Pair(int row, int col, int step){
+            this.row=row;
+            this.col=col;
+            this.step=step;
+        }
+    }
     public int[][] updateMatrix(int[][] mat) {
-        int m=mat.length;
-        int n=mat[0].length;
-        boolean vis[][]=new boolean[m][n];
-        int dist[][]= new int[m][n];
-        Queue<Node> queue= new LinkedList<>();
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+        int ans[][]= new int[mat.length][mat[0].length];
+        boolean vis[][]= new boolean[mat.length][mat[0].length];
+        Queue<Pair> queue= new LinkedList<>();
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat[0].length; j++){
                 if(mat[i][j]==0){
-                   queue.offer(new Node(i,j,0));
-                   vis[i][j]=true;
+                    vis[i][j]=true;
+                    queue.offer(new Pair(i,j,0));
                 }
             }
         }
-
-        int dir[][]={{0,-1},{0,1},{-1,0},{1,0}};
+        int dir[][]={{1,0},{-1,0},{0,1},{0,-1}};
         while(!queue.isEmpty()){
-            Node node= queue.poll();
-            int r=node.row;
-            int c=node.col;
-            int steps=node.step;
-            dist[r][c]=steps;
-            for(int []d:dir){
-                int newRow=r+d[0];
-                int newCol=c+d[1];
-                if(newRow>=0 && newCol>=0 && newRow<m && newCol<n && !vis[newRow][newCol]){
-                    vis[newRow][newCol]=true;
-                    queue.offer(new Node(newRow,newCol,steps+1));
+            Pair p= queue.poll();
+            int r=p.row;
+            int c=p.col;
+            int s=p.step;
+            ans[r][c]=s;
+            for(int d[]: dir){
+                int nR=r+d[0];
+                int nC=c+d[1];
+                if(nR>=0 && nC>=0 && nR<mat.length && nC<mat[0].length && !vis[nR][nC]){
+                    vis[nR][nC]=true;
+                    queue.offer(new Pair(nR,nC,s+1));
                 }
             }
-        }
-        return dist;
+        }return ans;
+
     }
 }
