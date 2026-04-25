6080e21eae72861ba12fe170a2f390d192909092
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-04-25 19:06:39 +0530

    Time: 372 ms (43.68%), Space: 42.8 MB (71.46%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean validNum(char [][]board, int row, int col,char c){
+    public boolean isValid(char [][]board, char c, int row, int col){
         for(int i=0; i<board.length; i++){
             if(board[row][i]==c) return false;
             if(board[i][col]==c) return false;
@@ -7,25 +7,24 @@ class Solution {
         }
         return true;
     }
-    public boolean solver(char[][] board) {
-        for(int row=0; row<board.length; row++){
-            for(int col=0; col<board.length; col++){
-                if(board[row][col]=='.'){
+    public void solveSudoku(char[][] board){
+        boolean s= solveSudoku2(board);
+    }
+    public boolean solveSudoku2(char[][] board) {
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                if(board[i][j]=='.'){
                     for(char c='1'; c<='9'; c++){
-                        if(validNum(board,row,col,c)){
-                            board[row][col]=c;
-                            if(solver(board)) return true;
-                            board[row][col]='.';
+                        if(isValid(board,c,i,j)){
+                            board[i][j]=c;
+                            if(solveSudoku2(board)) return true;
+                            board[i][j]='.';
                         }
                     }
-                    return false;
+                   return false;
                 }
             }
         }
         return true;
     }
-    public void solveSudoku(char[][] board){
-        boolean b=solver(board);
-    }
-
 }
