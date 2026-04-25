39832d05c99d71bb18f52c11e959be5489fdc7cc
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-04-25 15:52:06 +0530

    Time: 100 ms (13.98%), Space: 47.5 MB (34.52%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,37 +1,32 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int n=s.length();
-        int m=t.length();
-        if(n<m) return "";
-        int freq1[]= new int[126];
-        int freq2[]= new int[126];
+        if(t.length()>s.length()) return "";
+        int minLen=Integer.MAX_VALUE;
+        String minStr="";
+        int freq1[]=new int[128];
+        int freq2[]= new int[128];
         for(char c: t.toCharArray()){
             freq1[c]++;
         }
-        int req=0;
-        for(int i=0; i<126; i++){
-            if(freq1[i]>0) req++;
+        int required=0;
+        for(int i=0; i<128; i++){
+            if(freq1[i]>0) required++;
         }
-        int cur=0;
-        int right=0;
+        int curr=0;
         int left=0;
-        int minLen=Integer.MAX_VALUE;
-        String min="";
-        while(right<n){
+        for(int right=0; right<s.length(); right++){
             freq2[s.charAt(right)]++;
-            if(freq1[s.charAt(right)]==freq2[s.charAt(right)]) cur++;
-            while(left<=right && cur==req){
-                int len=right-left+1;
-                if(minLen>len){
-                    minLen=len;
-                    min=s.substring(left,right+1);
+            if(freq2[s.charAt(right)]==freq1[s.charAt(right)]) curr++;
+            while(left<=right && curr==required){
+                if(minLen>right-left+1){
+                    minLen=right-left+1;
+                    minStr=s.substring(left,right+1);
                 }
-                if(freq1[s.charAt(left)]==freq2[s.charAt(left)]) cur--;
+                if(freq2[s.charAt(left)]==freq1[s.charAt(left)]) curr--;
                 freq2[s.charAt(left)]--;
                 left++;
             }
-            right++;
         }
-        return min;
+        return minStr;
     }
 }
