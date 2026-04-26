9353f211252e15cc483796a6cd6727837a108d7c
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-04-26 18:02:26 +0530

    Time: 0 ms (100%), Space: 44.1 MB (83.95%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -1,55 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-
     public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+        ListNode prev=null;
+        ListNode after;
+        ListNode temp=head;
+        while(temp!=null){
+            after=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=after;
         }
-
         return prev;
-    }
-
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        if (head == null || left == right) return head;
-
-        ListNode prev = null;
-        ListNode leftNode = head;
-        ListNode rightNode = head;
-
-        // Move leftNode to left position
-        for (int i = 1; i < left; i++) {
-            prev = leftNode;
-            leftNode = leftNode.next;
-        }
-
-        // Move rightNode to right position
-        for (int i = 1; i < right; i++) {
-            rightNode = rightNode.next;
-        }
-
-        ListNode nextNode = rightNode.next;
-
-        // Cut only right side
-        rightNode.next = null;
-
-        // Reverse isolated sublist
-        ListNode reversedHead = reverseList(leftNode);
-
-        // Connect left part
-        if (prev != null) {
-            prev.next = reversedHead;
-        } else {
-            head = reversedHead;   // left == 1 case
-        }
-
-        // Connect right part
-        leftNode.next = nextNode;
-
-        return head;
     }
 }
