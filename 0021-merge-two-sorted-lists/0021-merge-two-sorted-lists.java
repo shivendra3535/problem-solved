7ce260d35653b32b00d2f0ed4dd2b14c568d1962
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-04-26 18:37:39 +0530

    Time: 0 ms (100%), Space: 44.4 MB (39.62%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -9,58 +9,46 @@
  * }
  */
 class Solution {
-    public ListNode merge(ListNode list1, ListNode list2)
-    { 
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if (list1 == null) return list2;
         if (list2 == null) return list1;
-
-
-        ListNode newHead;
-        ListNode left=list1;
-        ListNode right=list2;
-        ListNode newNode;
-        
-        if(left.val<=right.val){
-           newNode=left;
-           newHead=newNode;
-           left=left.next;
+        ListNode dummy= new ListNode(-1);
+        ListNode temp=dummy;
+        ListNode l1=list1;
+        ListNode l2=list2;
+        if(list1.val<list2.val){
+            temp.next=new ListNode(list1.val);
+            temp=temp.next;
+            l1=l1.next;
         }
-        else
-        {
-            newNode=right;
-            newHead=newNode;
-            right=right.next;
+        else{
+            temp.next=new ListNode(list2.val);
+            temp=temp.next;
+            l2=l2.next;
         }
-        while(left!=null&&right!=null)
-        {
-            if(left.val<=right.val)
-            {
-                newNode.next=left;
-                newNode=newNode.next;
-                left=left.next;
+       
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+               temp.next=new ListNode(l1.val);
+               temp=temp.next;
+               l1=l1.next;
             }
-            else
-            {
-                newNode.next=right;
-                newNode=newNode.next;
-                right=right.next;
+           else{
+               temp.next=new ListNode(l2.val);
+               temp=temp.next;
+               l2=l2.next;
             }
         }
-        while(left!=null)
-        {
-            newNode.next=left;
-            newNode=newNode.next;
-            left=left.next;
+        while(l1!=null){
+            temp.next= new ListNode(l1.val);
+            temp=temp.next;
+            l1=l1.next;
         }
-        while(right!=null)
-        {
-            newNode.next=right;
-            newNode=newNode.next;
-            right=right.next;
+        while(l2!=null){
+            temp.next=new ListNode(l2.val);
+            temp=temp.next;
+            l2=l2.next;
         }
-        return newHead;
-    }
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        return (merge(list1,list2));
+        return dummy.next;
     }
 }
