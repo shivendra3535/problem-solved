18b45a7fa5aa056021ce6da1d8ce1fad2f58a5e4
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-04-28 02:08:36 +0530

    Time: 2 ms (11.14%), Space: 47 MB (11.7%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -8,31 +8,23 @@ class Solution {
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n=image.length;
-        int m=image[0].length;
-        boolean vis[][] = new boolean[n][m];
-        vis[sr][sc]=true;
-        int original=image[sr][sc];
+
+        int prev=image[sr][sc];
+        if(prev==color) return image;
+        Queue<Pair> queue= new LinkedList<>();
+        int dir[][]={{-1,0},{1,0},{0,-1},{0,1}};
+        queue.offer(new Pair(sr,sc));
         image[sr][sc]=color;
-        Queue<Pair> q= new LinkedList<>();
-        q.offer(new Pair(sr,sc));
-        int dir[][]={
-            {-1,0},
-            {1,0},
-            {0,-1},
-            {0,1}
-        };
-        while(!q.isEmpty()){
-            Pair node=q.poll();
+        while(!queue.isEmpty()){
+            Pair node=queue.poll();
             int r=node.row;
             int c=node.col;
             for(int d[]: dir){
                 int nR=r+d[0];
                 int nC=c+d[1];
-                if(nR>=0 && nC>=0 && nR<n && nC<m && image[nR][nC]==original && !vis[nR][nC]){
-                    vis[nR][nC]=true;
+                if(nR>=0 && nC>=0 && nR<image.length && nC<image[0].length && image[nR][nC]==prev){
                     image[nR][nC]=color;
-                    q.offer(new Pair(nR,nC));
+                    queue.offer(new Pair(nR,nC));
                 }
             }
         }
