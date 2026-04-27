f60cb5af2b4a6b5476fdac89fa6c3bb3d42cd975
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-04-27 18:57:15 +0530

    Time: 0 ms (100%), Space: 46.3 MB (56.32%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -9,54 +9,48 @@
  * }
  */
 class Solution {
-    public ListNode reverse(ListNode head){
-        ListNode before=null;
-        ListNode curr=head;
-        ListNode after;
-        while(curr!=null){
-            after=curr.next;
-            curr.next=before;
-            before=curr;
-            curr=after;
-        }
-        return before;
-    }
-    public ListNode getKthNode(ListNode head, int k){
+    public ListNode kThNode(ListNode head, int k){
         ListNode temp=head;
         for(int i=1; i<k; i++){
-            if(temp.next==null){
-                return null;
-            }
+            if(temp==null) return null;
             temp=temp.next;
         }
         return temp;
     }
-    public ListNode reverseKGroup(ListNode head, int k) {
-        if(head==null || head.next==null){
-            return head;
-        }
+    public ListNode reverse(ListNode head){
         ListNode temp=head;
-        ListNode prevLast=null;
+        ListNode prev=null;
+        ListNode after;
         while(temp!=null){
-            ListNode kthNode= getKthNode(temp,k);
-            if(kthNode==null){
-                if(prevLast!=null){
-                    prevLast.next=temp;
+            after=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=after;
+        }
+        return prev;
+    }
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode curr=head;
+        ListNode prev=null;
+        while(curr!=null){
+            ListNode kth=kThNode(curr,k);
+            if(kth==null){
+                if(prev!=null){
+                    prev.next=curr;
                 }
                 break;
             }
- 
-            ListNode nextNode= kthNode.next;
-            kthNode.next=null;
-            ListNode newHead=reverse(temp);
-            if(temp==head){
+            ListNode nextNode=kth.next;
+            kth.next=null;
+            ListNode newHead=reverse(curr);
+            if(curr==head){
                 head=newHead;
             }
             else{
-                prevLast.next=newHead;
+                prev.next=newHead;
             }
-            prevLast=temp;
-            temp=nextNode;
+            prev=curr;
+            curr=nextNode;
         }
         return head;
     }
