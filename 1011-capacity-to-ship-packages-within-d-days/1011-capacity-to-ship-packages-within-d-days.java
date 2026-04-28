baa795808ebbab3b10e61f791e3831bb8dd21fd4
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-04-28 17:02:35 +0530

    Time: 11 ms (70.43%), Space: 50.3 MB (19.71%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,14 +1,14 @@
 class Solution {
-    public boolean isPossible(int weights[], int days, int mid){
+    public boolean isPossible(int [] weights, int days, int mid){
         int cnt=1;
-        int curWt=0;
-        for(int wt: weights){
-            if(curWt+wt<=mid){
-                curWt+=wt;
+        int cntWt=weights[0];
+        for(int i=1; i<weights.length; i++){
+            if(cntWt+weights[i]<=mid){
+                cntWt+=weights[i];
             }
             else{
                 cnt++;
-                curWt=wt;
+                cntWt=weights[i];
                 if(cnt>days) return false;
             }
         }
@@ -18,12 +18,12 @@ class Solution {
         int low=Integer.MIN_VALUE;
         int high=0;
         for(int wt: weights){
-            high+=wt;
             low=Math.max(low,wt);
+            high+=wt;
         }
         int ans=-1;
         while(low<=high){
-            int mid=(low+high)>>1;
+            int mid=low+(high-low)/2;
             if(isPossible(weights,days,mid)){
                 ans=mid;
                 high=mid-1;
