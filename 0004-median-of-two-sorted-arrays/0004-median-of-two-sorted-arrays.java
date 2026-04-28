5a05e6d2778fda4de73a8c46dcd01fd282a3796e
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-04-29 02:23:05 +0530

    Time: 1 ms (100%), Space: 48.9 MB (52.45%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,40 +1,25 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1=nums1.length;
-        int n2=nums2.length;
-        if(n1>n2) return findMedianSortedArrays(nums2, nums1);
-        int left=(n1+n2+1)/2;
-        int n=n1+n2;
+        int m=nums1.length;
+        int n=nums2.length;
+        int k=(m+n+1)/2;
+        int tot=m+n;
+        if(m>n) return findMedianSortedArrays(nums2,nums1);
         int low=0;
-        int high=n1;
+        int high=nums1.length;
         while(low<=high){
-            int mid1=(low+high)>>1;
-            int mid2=left-mid1;
+            int mid1=(low+high)/2;
+            int mid2=k-mid1;
 
-            int l1=Integer.MIN_VALUE;
-            int l2=Integer.MIN_VALUE;
+            int r1 = (mid1 < m) ? nums1[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2 < n) ? nums2[mid2] : Integer.MAX_VALUE;
 
-            int r1=Integer.MAX_VALUE;
-            int r2=Integer.MAX_VALUE;
-
-            if(mid1>0){
-                l1=nums1[mid1-1];
-            }
-            if(mid2>0){
-                l2=nums2[mid2-1];
-            }
-
-            if(mid1<n1){
-                r1=nums1[mid1];
-            }
-            if(mid2<n2){
-                r2=nums2[mid2];
-            }
-
+            int l1 = (mid1 > 0) ? nums1[mid1-1] : Integer.MIN_VALUE;
+            int l2 = (mid2 > 0) ? nums2[mid2-1] : Integer.MIN_VALUE;
 
             if(l1<=r2 && l2<=r1){
-                if(n%2!=0) return Math.max(l1,l2);
-                else return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+                if(tot%2!=0) return (double)Math.max(l1,l2);
+                else return (Math.max(l1,l2) + Math.min(r1,r2))/2.0;
             }
             else if(l1>r2){
                 high=mid1-1;
@@ -43,6 +28,6 @@ class Solution {
                 low=mid1+1;
             }
         }
-       return -1;
+       return 0;
     }
 }
