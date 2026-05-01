1c472033d9b01e58d8edf197f43baf9ddb3f641d
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-05-01 13:38:28 +0530

    Time: 1 ms (86.04%), Space: 45.5 MB (63.28%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,28 @@
 class Solution {
-    public void swap(int nums[],int i, int j){
+    public void swap(int nums[], int i, int j){
         int temp=nums[i];
         nums[i]=nums[j];
         nums[j]=temp;
     }
-    public void permute(int []nums, int index, List<List<Integer>> res){
-        if(index==nums.length){
-            List<Integer> numList = Arrays.stream(nums).boxed().collect(Collectors.toList());
-            res.add(new ArrayList<>(numList));
+    public void permutation(int nums[],int index,List<List<Integer>> res){
+        if(index>=nums.length){
+            List<Integer> ds= new ArrayList<>();
+            for(int n: nums){
+                ds.add(n);
+            }
+            res.add(new ArrayList<>(ds));
+            return;
         }
 
         for(int i=index; i<nums.length; i++){
-            swap(nums,i,index);
-            permute(nums,index+1,res);
-            swap(nums,i,index);
+            swap(nums,index,i);
+            permutation(nums,index+1,res);
+            swap(nums,index,i);
         }
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res= new ArrayList<>();
-        permute(nums,0,res);
+        permutation(nums,0,res);
         return res;
     }
 }
