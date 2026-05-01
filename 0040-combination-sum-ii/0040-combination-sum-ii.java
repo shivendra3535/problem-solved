d90d9110ba171ffba2c73b80a0790466803899d4
shivendra3535 <146531194+shivendra3535@users.noreply.github.com>
2026-05-01 14:21:06 +0530

    Time: 8 ms (14.94%), Space: 44.9 MB (87.5%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,26 +1,22 @@
 class Solution {
-    public void combinationSum2(int []candidates, int target,int index, List<List<Integer>> res, List<Integer> ds){
+    public void combination(int []candidates, int target, int index,List<List<Integer>> res, List<Integer> ds){
         if(target==0){
             res.add(new ArrayList<>(ds));
             return;
         }
-
         for(int i=index; i<candidates.length; i++){
             if(i>index && candidates[i]==candidates[i-1]) continue;
-            if(candidates[i]>target){
-                break;
-            }
+            if(target<0) break;
             ds.add(candidates[i]);
-            combinationSum2(candidates,target-candidates[i], i+1,res,ds);
+            combination(candidates,target-candidates[i],i+1,res,ds);
             ds.remove(ds.size()-1);
         }
-
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-         Arrays.sort(candidates);
-         List<List<Integer>> res= new ArrayList<>();
-         List<Integer> ds= new ArrayList<>();
-         combinationSum2(candidates,target,0,res,ds);
-         return res;
+        Arrays.sort(candidates);
+        List<List<Integer>> res= new ArrayList<>();
+        List<Integer> ds= new ArrayList<>();
+        combination(candidates,target,0,res,ds);
+        return res;
     }
 }
