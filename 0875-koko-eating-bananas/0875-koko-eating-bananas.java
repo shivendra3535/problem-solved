a27b0699ae8279a0e4af303fbee3c1e5d1eb805d
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-06-04 22:40:27 +0530

    Time: 6 ms (99.47%), Space: 47.8 MB (70.93%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,21 +1,21 @@
 class Solution {
-    public boolean isPossible(int[] piles, int h, int mid){
+    public boolean isPossible(int []piles, int h, int k){
         int cnt=0;
         for(int p: piles){
-            cnt+=(p+mid-1)/mid;
+            cnt+=(p+k-1)/k;
             if(cnt>h) return false;
         }
-        return true;
+        return cnt<=h;
     }
     public int minEatingSpeed(int[] piles, int h) {
         int low=1;
-        int high=Integer.MIN_VALUE;
-        int ans=-1;
+        int high=Integer.MAX_VALUE;
         for(int p: piles){
             high=Math.max(high,p);
         }
+        int ans=-1;
         while(low<=high){
-            int mid=low + (high-low)/2;
+            int mid=low+ (high-low)/2;
             if(isPossible(piles,h,mid)){
                 ans=mid;
                 high=mid-1;
@@ -24,6 +24,6 @@ class Solution {
                 low=mid+1;
             }
         }
-        return ans;
+       return ans;
     }
 }
