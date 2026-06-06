bc160ca45bf81ae9ac0320fdc589c785c919be6b
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-06-06 22:38:36 +0530

    Time: 7 ms (14.61%), Space: 51.4 MB (89.78%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,83 +1,42 @@
-import java.util.*;
-
 class Solution {
-
-    class Pair {
+    class Pair{
         int row;
         int col;
-
-        Pair(int row, int col) {
-            this.row = row;
-            this.col = col;
+        Pair(int row, int col){
+            this.row=row;
+            this.col=col;
         }
     }
-
-    public void bfs(int row, int col, char[][] grid, boolean[][] vis) {
-
-        int n = grid.length;
-        int m = grid[0].length;
-
-        Queue<Pair> queue = new LinkedList<>();
-
-        vis[row][col] = true;
+    public void bfs(char [][]grid, int row, int col, boolean vis[][]){
+        Queue<Pair> queue= new LinkedList<>();
+        vis[row][col]=true;
         queue.offer(new Pair(row, col));
-
-        int[][] dir = {
-            {0, -1},
-            {0, 1},
-            {-1, 0},
-            {1, 0}
-        };
-
-        while (!queue.isEmpty()) {
-
-            Pair node = queue.poll();
-
-            int r = node.row;
-            int c = node.col;
-
-            for (int[] d : dir) {
-
-                int nRow = r + d[0];
-                int nCol = c + d[1];
-
-                if (
-                    nRow >= 0 &&
-                    nRow < n &&
-                    nCol >= 0 &&
-                    nCol < m &&
-                    grid[nRow][nCol] == '1' &&
-                    !vis[nRow][nCol]
-                ) {
-                    vis[nRow][nCol] = true;
-                    queue.offer(new Pair(nRow, nCol));
+        int dir[][]={{-1,0},{1,0},{0,-1},{0,1}};
+        while(!queue.isEmpty()){
+            Pair p=queue.poll();
+            int r=p.row;
+            int c=p.col;
+            for(int d[]: dir){
+                int nr=r+d[0];
+                int nc=c+d[1];
+                if(nr<grid.length && nr>=0 && nc<grid[0].length && nc>=0 && grid[nr][nc]=='1' && !vis[nr][nc]){
+                    vis[nr][nc]=true;
+                    queue.offer(new Pair(nr,nc));
                 }
             }
         }
     }
-
     public int numIslands(char[][] grid) {
-
-        if (grid == null || grid.length == 0)
-            return 0;
-
-        int n = grid.length;
-        int m = grid[0].length;
-
-        boolean[][] vis = new boolean[n][m];
-
-        int cnt = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-
-                if (grid[i][j] == '1' && !vis[i][j]) {
+        int cnt=0;
+        boolean vis[][]= new boolean[grid.length][grid[0].length];
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j]=='1' && !vis[i][j]){
                     cnt++;
-                    bfs(i, j, grid, vis);
+                    bfs(grid,i,j,vis);
                 }
             }
         }
-
         return cnt;
     }
 }
