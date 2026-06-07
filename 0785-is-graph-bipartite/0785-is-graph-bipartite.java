ffebc80c76e9f6b28948697e5d691224b3760471
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-06-07 19:24:11 +0530

    Time: 0 ms (100%), Space: 47 MB (88.65%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,54 +1,22 @@
 class Solution {
-    class Pair{
-        int node;
-        int color;
-        Pair(int node,int color){
-            this.node=node;
-            this.color=color;
-        }
-    }
-    public boolean dfs(int graph[][],int node ,boolean vis[],int colors[], int color){
+    public boolean dfs(int node,int [][] graph, boolean vis[], int colors[], int color){
         vis[node]=true;
         colors[node]=color;
         int nextColor=1-color;
         for(int a: graph[node]){
             if(!vis[a]){
-                if(!dfs(graph,a,vis,colors,nextColor)) return false;
+                if(!dfs(a,graph,vis,colors,nextColor)) return false;
             }
-            else if(colors[a]==color){
-                return false;
-            }
-        }
-        return true;
-    }
-    public boolean bfs(int graph[][],int node ,boolean vis[],int colors[], int color){
-        vis[node]=true;
-        colors[node]=color;
-        Queue<Pair> queue= new LinkedList<>();
-        queue.offer(new Pair(node,color));
-        while(!queue.isEmpty()){
-            Pair n=queue.poll();
-            int nextColor=1-n.color;
-            for(int a: graph[n.node]){
-                if(!vis[a]){
-                    vis[a]=true;
-                    colors[a]=nextColor;
-                    queue.offer(new Pair(a,nextColor));
-                }
-                else if(colors[a]==n.color){
-                    return false;
-                }
-            }
+            else if(colors[a]==color) return false;
         }
         return true;
     }
     public boolean isBipartite(int[][] graph) {
-        int colors[]= new int[graph.length];
         boolean vis[]= new boolean[graph.length];
-        Arrays.fill(colors,-1);
+        int colors[]= new int[graph.length];
         for(int i=0; i<graph.length; i++){
             if(!vis[i]){
-                if(!bfs(graph,i,vis,colors,0)) return false;
+                if(!dfs(i,graph,vis,colors,0)) return false;
             }
         }
         return true;
