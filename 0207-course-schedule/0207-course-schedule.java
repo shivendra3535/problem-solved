d1d27a7ffc4f19453dab5537081d64c2ccb5f879
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-06-15 21:27:09 +0530

    Time: 7 ms (58.29%), Space: 47 MB (76.7%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,34 +1,27 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj= new ArrayList<>();
-        int indegree[]= new int[numCourses];
+        Queue<Integer> queue= new LinkedList<>();
         for(int i=0; i<numCourses; i++){
             adj.add(new ArrayList<>());
         }
-        for(int e[]: prerequisites){
-            int u=e[0];
-            int v=e[1];
-            indegree[v]++;
-            adj.get(u).add(v);
+        int outG[]= new int[numCourses];
+        for(int p[]: prerequisites){
+            int u=p[0];
+            int v=p[1];
+            outG[u]++;
+            adj.get(v).add(u);
         }
-        Queue<Integer> queue= new LinkedList<>();
+        for(int i=0; i<numCourses; i++) if(outG[i]==0) queue.offer(i);
         int cnt=0;
-        for(int i=0; i<numCourses; i++){
-            if(indegree[i]==0){
-               queue.offer(i);
-            }
-        }
         while(!queue.isEmpty()){
-            int cur=queue.poll();
             cnt++;
-            for(int a: adj.get(cur)){
-                indegree[a]--;
-                if(indegree[a]==0){
-                    queue.offer(a);
-                }
+            int node=queue.poll();
+            for(int a: adj.get(node)){
+                outG[a]--;
+                if(outG[a]==0) queue.offer(a);
             }
         }
-        if(cnt==numCourses) return true;
-        return false;
+        return cnt==numCourses;
     }
 }
