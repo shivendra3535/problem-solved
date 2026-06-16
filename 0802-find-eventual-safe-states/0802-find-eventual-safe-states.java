89c84107dac1eab96c75f14d840881e05735f5da
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-06-16 16:31:05 +0530

    Time: 27 ms (32.05%), Space: 62.1 MB (52.64%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -13,18 +13,30 @@ class Solution {
         return false;
     }
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        boolean vis[]= new boolean[graph.length];
-        boolean pathVis[]= new boolean[graph.length];
-        boolean safe[]= new boolean[graph.length];
         List<Integer> res= new ArrayList<>();
-        for(int i=0; i<graph.length; i++){
-            if(!vis[i]){
-                boolean cycle= dfs(graph,vis,pathVis,safe,i);
+        List<List<Integer>> adj= new ArrayList<>();
+        int V= graph.length;
+        int inorder[]= new int[V];
+        for(int i=0; i<V; i++) adj.add(new ArrayList<>());
+        for(int i=0; i<V; i++){
+            for(int a: graph[i]){
+                adj.get(a).add(i);
+                inorder[i]++;
             }
         }
-        for(int i=0; i<graph.length; i++){
-            if(safe[i]) res.add(i);
+        Queue<Integer> queue= new LinkedList<>();
+        for(int i=0; i<V; i++){
+            if(inorder[i]==0) queue.offer(i);
         }
+        while(!queue.isEmpty()){
+            int node=queue.poll();
+            res.add(node);
+            for(int a: adj.get(node)){
+                inorder[a]--;
+                if(inorder[a]==0) queue.offer(a);
+            }
+        }
+        Collections.sort(res);
         return res;
     }
 }
