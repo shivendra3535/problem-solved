82f59c2da2e0cd5dae0787200fa4b501e085d0ba
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-06-30 14:07:00 +0530

    Time: 4 ms (99.72%), Space: 43.8 MB (92.89%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,33 +1,35 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int n=s1.length();
-        int m=s2.length();
-        if(n>m) return false;
-        int freq1[]= new int[26];
-        int freq2[]= new int[26];
+        int l1=s1.length();
+        int l2=s2.length();
+        if(l1>l2) return false;
+        int freq1[]=new int[26];
+        int freq2[]=new int[26];
         for(char c: s1.toCharArray()){
             freq1[c-'a']++;
         }
-        int required=0;
+        int req=0;
         for(int i=0; i<26; i++){
-            if(freq1[i]>0) required++;
+            if(freq1[i]>0) req++;
         }
-
-        int curMatch=0;
-        int right=0;
         int left=0;
-        while(right<s1.length()){
-            freq2[s2.charAt(right)-'a']++;
-            if(freq2[s2.charAt(right)-'a']==freq1[s2.charAt(right)-'a']) curMatch++;
+        int right=0;
+        int curr=0;
+        while(right<l1){
+            int a=s2.charAt(right)-'a';
+            freq2[a]++;
+            if(freq1[a]==freq2[a]) curr++;
             right++;
         }
-        if(curMatch==required) return true;
-        while(right<s2.length()){
-            freq2[s2.charAt(right)-'a']++;
-            if(freq2[s2.charAt(right)-'a']==freq1[s2.charAt(right)-'a']) curMatch++;
-            if(freq2[s2.charAt(left)-'a']==freq1[s2.charAt(left)-'a']) curMatch--;
-            freq2[s2.charAt(left)-'a']--;
-            if(curMatch==required) return true;
+        if(curr==req) return true;
+        while(right<l2){
+            int a=s2.charAt(right)-'a';
+            freq2[a]++;
+            if(freq1[a]==freq2[a]) curr++;
+            int b=s2.charAt(left)-'a';
+            if(freq1[b]==freq2[b]) curr--;
+            freq2[b]--;
+            if(curr==req) return true;
             right++;
             left++;
         }
