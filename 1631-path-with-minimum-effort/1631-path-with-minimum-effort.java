1b12529813493cc99c5eef992585790ec948fc3e
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-06-30 21:14:29 +0530

    Time: 57 ms (26.42%), Space: 48.3 MB (7.35%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,42 +1,42 @@
 class Solution {
-    class Triple{
+    class Tripple{
         int row;
         int col;
-        int effort;
-        Triple(int row,int col, int effort){
+        int efforts;
+        Tripple(int row, int col, int efforts){
             this.row=row;
             this.col=col;
-            this.effort=effort;
+            this.efforts=efforts;
         }
     }
     public int minimumEffortPath(int[][] heights) {
         int m=heights.length;
         int n=heights[0].length;
-        int diff[][]= new int[m][n];
+        int dist[][]=new int[m][n];
         for(int i=0; i<m; i++){
-            Arrays.fill(diff[i],Integer.MAX_VALUE);
+            Arrays.fill(dist[i],Integer.MAX_VALUE);
         }
-        diff[0][0]=0;
-        PriorityQueue<Triple> pq= new PriorityQueue<>((a,b)->a.effort-b.effort);
-        pq.offer(new Triple(0,0,0));
-        int dir[][]={{0,1},{0,-1},{1,0},{-1,0}};
+        dist[0][0]=0;
+        int dir[][]={{-1,0},{1,0},{0,-1},{0,1}};
+        PriorityQueue<Tripple> pq= new PriorityQueue<>((a,b)->a.efforts-b.efforts);
+        pq.offer(new Tripple(0,0,0));
         while(!pq.isEmpty()){
-            Triple p= pq.poll();
-            int row=p.row;
-            int col=p.col;
-            int effort=p.effort;
-            for(int d[]:dir){
-                int nR=row+d[0];
-                int nC=col+d[1];
+            Tripple t=pq.poll();
+            int r=t.row;
+            int c=t.col;
+            int currEffort=t.efforts;
+            for(int d[]: dir){
+                int nR=r+d[0];
+                int nC=c+d[1];
                 if(nR>=0 && nC>=0 && nR<m && nC<n){
-                    int newEffort=Math.max(effort,Math.abs(heights[nR][nC]-heights[row][col]));
-                    if(newEffort<diff[nR][nC]){
-                        diff[nR][nC]=newEffort;
-                        pq.offer(new Triple(nR,nC,newEffort));
+                    int newEffort=Math.max(currEffort,Math.abs(heights[nR][nC]-heights[r][c]));
+                    if(newEffort<dist[nR][nC]){
+                        dist[nR][nC]=newEffort;
+                        pq.offer(new Tripple(nR,nC,newEffort));
                     }
                 }
-            }
+            }  
         }
-        return diff[m-1][n-1];
+        return dist[m-1][n-1];
     }
 }
