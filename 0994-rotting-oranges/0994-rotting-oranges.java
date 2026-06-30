d528b402365d1e9ac38418b3eaa9716ef760fc29
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-06-30 16:13:16 +0530

    Time: 2 ms (90.75%), Space: 44.4 MB (24.64%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -8,40 +8,40 @@ class Solution {
         }
     }
     public int orangesRotting(int[][] grid) {
-        int fresh=0;
+        boolean vis[][]= new boolean[grid.length][grid[0].length];
+        int freshCnt=0;
+        int min=-1;
         Queue<Pair> queue= new LinkedList<>();
         for(int i=0; i<grid.length; i++){
             for(int j=0; j<grid[0].length; j++){
                 if(grid[i][j]==2){
+                    vis[i][j]=true;
                     queue.offer(new Pair(i,j));
                 }
-                if(grid[i][j]==1){
-                    fresh++;
-                }
+                else if(grid[i][j]==1) freshCnt++;
             }
         }
-        if(fresh==0) return 0;
-        int cnt=-1;
-        int dir[][]={{-1,0},{1,0},{0,1},{0,-1}};
+        if(freshCnt==0) return 0;
+        int dir[][]={{1,0},{-1,0},{0,1},{0,-1}};
         while(!queue.isEmpty()){
+            min++;
             int size=queue.size();
-            cnt++;
             for(int i=0; i<size; i++){
-                Pair p=queue.poll();
-                int r= p.row;
-                int c= p.col;
+                Pair p= queue.poll();
+                int r=p.row;
+                int c=p.col;
                 for(int d[]: dir){
-                    int nR= r+d[0];
-                    int nC= c+d[1];
-                    if(nR>=0 && nC>=0 && nR<grid.length && nC<grid[0].length && grid[nR][nC]==1){
-                        grid[nR][nC]=2;
-                        fresh--;
-                        queue.offer(new Pair(nR,nC));
+                    int nR=r+d[0];
+                    int nC=c+d[1];
+                    if(nR>=0 && nC>=0 && nR<grid.length && nC<grid[0].length && !vis[nR][nC] && grid[nR][nC]==1){
+                       vis[nR][nC]=true;
+                       freshCnt--;
+                       queue.offer(new Pair(nR,nC));
                     }
                 }
             }
         }
-        if(fresh>0) return -1;
-        return cnt;
+        if(freshCnt!=0) return -1;
+        return min;
     }
 }
