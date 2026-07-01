312989553605c872b72287c78d572b3d7635bfc5
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-07-01 13:32:42 +0530

    Time: 103 ms (14.25%), Space: 47.8 MB (25.01%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,30 +1,32 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(t.length()>s.length()) return "";
-        int minLen=Integer.MAX_VALUE;
-        String minStr="";
-        int freq1[]=new int[128];
-        int freq2[]= new int[128];
-        for(char c: t.toCharArray()){
-            freq1[c]++;
+        int l1=s.length();
+        int l2=t.length();
+        if(l1<l2) return "";
+        int freq1[]=new int[126];
+        int freq2[]=new int[126];
+        for(int i=0; i<l2; i++){
+            freq1[t.charAt(i)]++;
         }
         int required=0;
-        for(int i=0; i<128; i++){
+        for(int i=0; i<126; i++){
             if(freq1[i]>0) required++;
         }
-        int curr=0;
+        int minLen=Integer.MAX_VALUE;
+        String minStr="";
         int left=0;
-        for(int right=0; right<s.length(); right++){
+        int curr=0;
+        for(int right=0; right<l1; right++){
             freq2[s.charAt(right)]++;
             if(freq2[s.charAt(right)]==freq1[s.charAt(right)]) curr++;
             while(left<=right && curr==required){
-                if(minLen>right-left+1){
+                if(right-left+1<minLen){
                     minLen=right-left+1;
                     minStr=s.substring(left,right+1);
                 }
                 if(freq2[s.charAt(left)]==freq1[s.charAt(left)]) curr--;
                 freq2[s.charAt(left)]--;
-                left++;
+                left++; 
             }
         }
         return minStr;
