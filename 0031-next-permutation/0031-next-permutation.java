0a8f58d26b752ee3e8067c1e4f66196f9e815ca3
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-07-04 20:34:12 +0530

    Time: 2 ms (7.98%), Space: 44.7 MB (31.94%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,7 +1,17 @@
 class Solution {
+    public void rev(int []nums, int i, int j){
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+    }
     public void nextPermutation(int[] nums) {
         int index=-1;
-        for(int i=nums.length-2; i>=0; i--){
+        int n=nums.length;
+        for(int i=n-2; i>=0; i--){
             if(nums[i]<nums[i+1]){
                 index=i;
                 break;
@@ -11,7 +21,7 @@ class Solution {
             Arrays.sort(nums);
             return;
         }
-        for(int i=nums.length-1; i>=i; i--){
+        for(int i=n-1; i>index; i--){
             if(nums[i]>nums[index]){
                 int temp=nums[i];
                 nums[i]=nums[index];
@@ -19,14 +29,6 @@ class Solution {
                 break;
             }
         }
-        int left=index+1;
-        int right=nums.length-1;
-        while(left<right){
-            int temp=nums[left];
-            nums[left]=nums[right];
-            nums[right]=temp;
-            left++;
-            right--;
-        }
+        rev(nums,index+1,n-1);
     }
 }
