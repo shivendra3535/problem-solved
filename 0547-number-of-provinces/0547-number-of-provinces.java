a28c9c104b5187983a1fa91fdf4da65811be2402
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-07-05 14:36:16 +0530

    Time: 3 ms (26.87%), Space: 47.1 MB (73.57%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,4 +1,40 @@
+class DisjointSet{
+    ArrayList<Integer> rank= new ArrayList<>();
+    ArrayList<Integer> parent= new ArrayList<>();
+    public DisjointSet(int n){
+        for(int i=0; i<n; i++){
+            rank.add(0);
+            parent.add(i);
+        }
+    }
+    
+    public int findUPar(int node){
+        if(node==parent.get(node)) return node;
+        
+        int ulp=findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+    
+    public void unionByRank(int u, int v){
+        int ulpu=findUPar(u);
+        int ulpv=findUPar(v);
+        if(ulpu==ulpv) return;
+        if(rank.get(ulpv)<rank.get(ulpu)){
+            parent.set(ulpv,ulpu);
+        }
+        else if(rank.get(ulpu)<rank.get(ulpv)){
+            parent.set(ulpu,ulpv);
+        }
+        else{
+            parent.set(ulpv, ulpu);
+            int rankU=rank.get(ulpu);
+            rank.set(ulpu, rankU+1);
+        }
+    }
+}
 class Solution {
+    /*
     public void dfs(int node, boolean vis[],int[][] isConnected){
         vis[node]=true;
         for(int i=0; i<isConnected[0].length; i++){
@@ -7,7 +43,9 @@ class Solution {
             }
         }
     }
+    */
     public int findCircleNum(int[][] isConnected) {
+        /*
         boolean vis[]= new boolean[isConnected.length];
         int cnt=0;
         for(int i=0; i<isConnected.length; i++){
@@ -15,6 +53,20 @@ class Solution {
                 cnt++;
                 dfs(i,vis,isConnected);
             }
+        }
+        return cnt; */
+        int n=isConnected.length;
+        DisjointSet dj= new DisjointSet(n);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i!=j && isConnected[i][j]==1){
+                    dj.unionByRank(i,j);
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=0; i<n; i++){
+            if(dj.parent.get(i)==i) cnt++;
         }
         return cnt;
     }
