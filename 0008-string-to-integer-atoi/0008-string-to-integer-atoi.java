4d78cdfdd3d3065ea64b5872b38ba4b56b740f9f
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-07-24 22:54:57 +0530

    Time: 1 ms (100%), Space: 43.9 MB (41.62%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,25 +1,22 @@
 class Solution {
     public int myAtoi(String s) {
         int i=0;
-        long nums=0;
+        long n=0;
         int sign=1;
-        while(i<s.length() && s.charAt(i)==' '){
-            i++;
-        }
-        while(i<s.length() && (s.charAt(i)=='+' || s.charAt(i)=='-')){
+        while(i<s.length() && s.charAt(i)==' ') i++;
+        while(i<s.length() && (s.charAt(i)=='-' || s.charAt(i)=='+')){
             if(i>0 && (s.charAt(i-1)=='+' || s.charAt(i-1)=='-')) return 0;
-            if(s.charAt(i)=='-') sign=-1;
+            if(s.charAt(i)=='-') sign=-1*sign;
             i++;
         }
-
         while(i<s.length() && Character.isDigit(s.charAt(i))){
-            nums= nums*10 + s.charAt(i)-'0';
-
-            if(sign==1 && nums>Integer.MAX_VALUE) return Integer.MAX_VALUE;
-            if(sign==-1 && nums>Integer.MAX_VALUE) return Integer.MIN_VALUE;
-
+            n=10*n+s.charAt(i)-'0';
+            if(n>Integer.MAX_VALUE){
+                if(sign==-1) return Integer.MIN_VALUE;
+                return Integer.MAX_VALUE;
+            }
             i++;
         }
-        return sign*(int)nums;
+        return sign*(int)n;
     }
 }
