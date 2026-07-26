7dcd3734dda5a3078f6560262aa4621736e1e45e
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-07-26 23:12:11 +0530

    Time: 17 ms (42.61%), Space: 47.8 MB (46.72%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,39 +1,23 @@
 class Solution {
-    class Triple{
-        int distance;
-        int row;
-        int col;
-        Triple(int distance, int row, int col){
-            this.distance=distance;
-            this.row=row;
-            this.col=col;
-        }
-    }
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int n=grid[0].length;
-        if(grid[0][0]==1 || grid[n-1][n-1]==1) return -1;
-        int dist[][]= new int[n][n];
-        PriorityQueue<Triple> pq= new PriorityQueue<>((a,b)->a.distance-b.distance);
-        pq.offer(new Triple(1,0,0));
-        for(int i=0; i<n; i++){
-            Arrays.fill(dist[i],Integer.MAX_VALUE);
-        }
-        dist[0][0]=1;
-        int dir[][]={{0,-1},{0,1},{-1,0},{1,0},{-1,-1},{1,1},{-1,1},{1,-1}};
-        while(!pq.isEmpty()){
-            Triple top=pq.poll();
-            int distance=top.distance;
-            int row=top.row;
-            int col=top.col;
-            if(row==n-1 && col==n-1) return distance;
+        if(grid[0][0]==1 || grid[grid.length-1][grid.length-1]==1) return -1;
+        boolean vis[][]= new boolean[grid.length][grid.length];
+        Queue<int[]> queue= new LinkedList<>();
+        vis[0][0]=true;
+        queue.offer(new int[]{0,0,1});
+        int dir[][]={{0,1},{0,-1},{1,0},{-1,0},{-1,-1},{1,1},{1,-1},{-1,1}};
+        while(!queue.isEmpty()){
+            int top[]=queue.poll();
+            int r=top[0];
+            int c=top[1];
+            int dist=top[2];
+            if(r==grid.length-1 && c==grid.length-1) return dist;
             for(int d[]: dir){
-                int nR=row+d[0];
-                int nC=col+d[1];
-                if(nR>=0 && nC>=0 && nR<n && nC<n && grid[nR][nC]==0){
-                    if(distance+1<dist[nR][nC]){
-                        dist[nR][nC]=distance+1;
-                        pq.offer(new Triple(dist[nR][nC],nR,nC));
-                    }
+                int nr=r+d[0];
+                int nc=c+d[1];
+                if(nr>=0 && nc>=0 && nr<grid.length && nc<grid.length && !vis[nr][nc] && grid[nr][nc]==0){
+                    vis[nr][nc]=true;
+                    queue.offer(new int[]{nr,nc,dist+1});
                 }
             }
         }
