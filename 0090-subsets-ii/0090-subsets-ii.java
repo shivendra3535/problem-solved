bec7f9e59a192d1353a10c0104a3737ff7fc27c7
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-07-26 22:50:39 +0530

    Time: 2 ms (99.7%), Space: 45 MB (77.56%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,12 +1,10 @@
 class Solution {
-    public void subset(int []nums, int index, List<List<Integer>> res, List<Integer> ds){
+    public void subset(int nums[], int index, List<Integer> ds, List<List<Integer>> res){
         res.add(new ArrayList<>(ds));
-        
         for(int i=index; i<nums.length; i++){
             if(i>index && nums[i]==nums[i-1]) continue;
-
             ds.add(nums[i]);
-            subset(nums,i+1,res,ds);
+            subset(nums,i+1, ds, res);
             ds.remove(ds.size()-1);
         }
     }
@@ -14,7 +12,7 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> res= new ArrayList<>();
         List<Integer> ds= new ArrayList<>();
-        subset(nums,0,res,ds);
+        subset(nums,0,ds,res);
         return res;
     }
 }
