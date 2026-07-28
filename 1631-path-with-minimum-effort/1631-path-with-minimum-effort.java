bbb11cf24e46d53430bbea8cfc82edef75ed84c9
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-07-28 15:09:01 +0530

    Time: 56 ms (29.64%), Space: 48.3 MB (7.93%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,41 +1,41 @@
 class Solution {
-    class Tripple{
+    class Tup{
         int row;
         int col;
-        int efforts;
-        Tripple(int row, int col, int efforts){
+        int effort;
+        Tup(int row, int col, int effort){
             this.row=row;
             this.col=col;
-            this.efforts=efforts;
+            this.effort=effort;
         }
     }
     public int minimumEffortPath(int[][] heights) {
         int m=heights.length;
         int n=heights[0].length;
-        int dist[][]=new int[m][n];
+        int dist[][]= new int[m][n];
         for(int i=0; i<m; i++){
             Arrays.fill(dist[i],Integer.MAX_VALUE);
         }
         dist[0][0]=0;
-        int dir[][]={{-1,0},{1,0},{0,-1},{0,1}};
-        PriorityQueue<Tripple> pq= new PriorityQueue<>((a,b)->a.efforts-b.efforts);
-        pq.offer(new Tripple(0,0,0));
+        PriorityQueue<Tup> pq= new PriorityQueue<>((a,b)->a.effort-b.effort);
+        pq.offer(new Tup(0,0,0));
+        int dir[][]={{0,1},{0,-1},{1,0},{-1,0}};
         while(!pq.isEmpty()){
-            Tripple t=pq.poll();
-            int r=t.row;
-            int c=t.col;
-            int currEffort=t.efforts;
+            Tup p=pq.poll();
+            int r=p.row;
+            int c=p.col;
+            int ef=p.effort;
             for(int d[]: dir){
-                int nR=r+d[0];
-                int nC=c+d[1];
-                if(nR>=0 && nC>=0 && nR<m && nC<n){
-                    int newEffort=Math.max(currEffort,Math.abs(heights[nR][nC]-heights[r][c]));
-                    if(newEffort<dist[nR][nC]){
-                        dist[nR][nC]=newEffort;
-                        pq.offer(new Tripple(nR,nC,newEffort));
+                int nr=d[0]+r;
+                int nc=d[1]+c;
+                if(nr>=0 && nr<m && nc>=0 && nc<n){
+                    int newEffort=Math.max(ef,Math.abs(heights[nr][nc]-heights[r][c]));
+                    if(newEffort<dist[nr][nc]){
+                        dist[nr][nc]=newEffort;
+                        pq.offer(new Tup(nr,nc,newEffort));
                     }
                 }
-            }  
+            }
         }
         return dist[m-1][n-1];
     }
