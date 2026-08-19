6c094d4d7d3afba233dab5fe897b2206af12e309
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-08-19 23:44:23 +0530

    Time: 0 ms (100%), Space: 42 MB (73.55%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int recur(int n, int dp[]){
+        if(n<=1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=recur(n-1, dp)+ recur(n-2,dp);
+        return dp[n];
+    }
+    public int tabulation(int n){
+        int dp[]= new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    public int climbStairs(int n) {
+        int dp[]= new int[n+1];
+        Arrays.fill(dp,-1);
+        return recur(n,dp);
+    }
+}
