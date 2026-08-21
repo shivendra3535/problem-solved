36ba98b3272a84167ebe9384f785d754700e5bb8
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-08-21 21:40:23 +0530

    Time: 0 ms (100%), Space: 45 MB (23.01%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -7,11 +7,23 @@ class Solution {
         dp[index]=Math.min(first,second);
         return dp[index];
     }
+    public int tabulation(int cost[], int index){
+        int dp[]= new int[cost.length];
+        for(int i=cost.length-1; i>=index; i--){
+            int first=cost[i]+ (i+1>=cost.length?0:dp[i+1]);
+            int second=cost[i]+ (i+2>=cost.length?0:dp[i+2]);
+            dp[i]=Math.min(first,second);
+        }
+        return dp[index];
+    }
     public int minCostClimbingStairs(int[] cost) {
+        /* Memoization
         int dp[]= new int[cost.length];
         Arrays.fill(dp,-1);
         int first=memoization(cost,0,dp);
         int second=memoization(cost,1,dp);
-        return Math.min(first,second);
+        return Math.min(first,second); */
+
+        return Math.min(tabulation(cost,0),tabulation(cost,1));
     }
 }
