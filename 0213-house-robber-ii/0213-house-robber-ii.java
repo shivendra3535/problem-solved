9f491c4337bb87ca3ff68c936f2fbbac86705839
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-08-21 20:05:34 +0530

    Time: 0 ms (100%), Space: 42.9 MB (41.94%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -7,7 +7,18 @@ class Solution {
         dp[index]=Math.max(take,notTake);
         return dp[index];
     }
+    public int tabulation(int nums[], int start, int end){
+        int dp[]= new int[nums.length];
+        for(int i=end; i>=start; i--){
+            int take= nums[i]+ ((i+2)>end?0:dp[i+2]);
+            int notTake=(i+1)>end?0:dp[i+1];
+            dp[i]=Math.max(take,notTake);
+        }
+        return dp[start];
+    }
     public int rob(int[] nums) {
+        if(nums.length==1) return nums[0];
+        /*
         if(nums.length==1) return nums[0];
         int dp1[]= new int[nums.length];
         int dp2[]= new int[nums.length];
@@ -16,6 +27,10 @@ class Solution {
 
         int first=robbery(nums,0,dp1,nums.length-2);
         int second=robbery(nums,1,dp2,nums.length-1);
+        return Math.max(first,second);*/
+        int first=tabulation(nums,0,nums.length-2);
+        int second=tabulation(nums,1,nums.length-1);
         return Math.max(first,second);
+        
     }
 }
