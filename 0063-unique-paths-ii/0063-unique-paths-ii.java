e357e3b8d2f36890e3f054936561d3027964570e
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-08-26 01:20:50 +0530

    Time: 0 ms (100%), Space: 43.9 MB (11.04%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -8,11 +8,38 @@ class Solution {
         dp[m][n]=up+left;
         return dp[m][n];
     }
+    public int tabulation(int[][] obstacleGrid){
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        int dp[][]= new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                    continue;
+                }
+                
+                if(i==0 && j==0){
+                    dp[0][0]=1;
+                    continue;
+                }
+                else{
+                    int up=0;
+                    int left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int dp[][]= new int[obstacleGrid.length][obstacleGrid[0].length];
+        /*int dp[][]= new int[obstacleGrid.length][obstacleGrid[0].length];
         for(int i=0; i<obstacleGrid.length; i++){
             Arrays.fill(dp[i],-1);
         }
-        return uniquePath(obstacleGrid,obstacleGrid.length-1,obstacleGrid[0].length-1,dp);
+        return uniquePath(obstacleGrid,obstacleGrid.length-1,obstacleGrid[0].length-1,dp); */
+        return tabulation(obstacleGrid);
     }
 }
