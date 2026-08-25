87ba6c58efe9579deda684afdf5bc20c3d299f7d
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-08-25 22:46:34 +0530

    Time: 1 ms (14.42%), Space: 42.4 MB (12.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -11,11 +11,33 @@ class Solution {
         dp[m][n]=left+up;
         return dp[m][n];
     }
-    public int uniquePaths(int m, int n) {
+    public int tabulation(int m, int n){
         int dp[][]= new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                else{
+                    int up=0;
+                    int left=0;
+
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    public int uniquePaths(int m, int n) {
+        /*int dp[][]= new int[m][n];
         for(int i=0; i<m; i++){
             Arrays.fill(dp[i],-1);
         }
-        return uniquePaths2(m-1,n-1,dp);
+        return uniquePaths2(m-1,n-1,dp); */
+
+        return tabulation(m,n);
     }
 }
