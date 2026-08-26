c9bb8dfe847e581894b34646d9f7e15f68065340
Shivendra Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-08-26 20:26:09 +0530

    Time: 6 ms (10.07%), Space: 49.8 MB (95.85%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -11,13 +11,33 @@ class Solution {
         dp[m][n]= Math.min(up,left);
         return dp[m][n];
     }
+    public int tabulation(int grid[][], int m, int n){
+        int dp[][]= new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0){
+                    dp[0][0]=grid[0][0];
+                    continue;
+                }
+                else{
+                    int up=(int)1e9;
+                    int left=(int)1e9;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=grid[i][j]+Math.min(up,left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
     public int minPathSum(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        int dp[][]= new int[m][n];
+        /*int dp[][]= new int[m][n];
         for(int i=0; i<m; i++){
             Arrays.fill(dp[i],-1);
         }
-        return minPath(grid,grid.length-1, grid[0].length-1,dp);
+        return minPath(grid,grid.length-1, grid[0].length-1,dp); */
+        return tabulation(grid,m,n);
     }
 }
