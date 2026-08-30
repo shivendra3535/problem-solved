543c87ff62f27a7fe42a0940e15c61f231aca65b
Shivendra Rajat Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-08-30 14:02:16 +0530

    Time: 50 ms (49.88%), Space: 47.9 MB (45.8%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -14,6 +14,29 @@ class Solution {
         dp[index][target]=pick || notPick;
         return dp[index][target];
     }
+    public boolean tabulation(int nums[], int n, int target){
+        boolean dp[][]= new boolean[n][target+1];
+        for(int i=0; i<n; i++){
+            dp[i][0]=true;
+        }
+        if(nums[0]<=target){
+            dp[0][nums[0]]=true;
+        }
+        for(int index=1; index<n; index++){
+            for(int tgt=1; tgt<=target; tgt++){
+                boolean notPick=dp[index-1][tgt];
+                
+                boolean pick = false;
+
+                if (nums[index] <= tgt) {
+                    pick = dp[index - 1][tgt - nums[index]];
+                }
+                
+                dp[index][tgt]= pick || notPick;
+            }
+        }
+        return dp[n-1][target];
+    }
     public boolean canPartition(int[] nums) {
         int sum=0;
         for(int n: nums){
@@ -21,7 +44,8 @@ class Solution {
         }
         if(sum%2!=0) return false;
         int target = sum / 2;
-        Boolean dp[][]= new Boolean[nums.length][target+1];
-        return memoization(nums,nums.length-1,sum/2,dp);
+        /*Boolean dp[][]= new Boolean[nums.length][target+1];
+        return memoization(nums,nums.length-1,sum/2,dp);*/
+        return tabulation(nums,nums.length,target);
     }
 }
