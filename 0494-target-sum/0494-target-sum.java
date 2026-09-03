02c00647f535ee89deb97258936e48bad97a165f
Shivendra Rajat Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-09-03 07:39:02 +0530

    Time: 8 ms (60.17%), Space: 46.9 MB (23.1%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,14 +1,30 @@
 class Solution {
-    public int targetSum(int []nums, int target, int index){
+    public int targetSum(int []nums, int target, int index, int dp[][], int offset){
         if(index>=nums.length){
             if(target==0) return 1;
             return 0;
         }
-        int add=targetSum(nums,target-nums[index], index+1);
-        int sub=targetSum(nums,target+nums[index],index+1);
-        return add+sub;
+        if (target > offset || target < -offset) {
+             return 0;
+         }
+        if(dp[index][target+offset]!=-1) return dp[index][target+offset];
+        int add=targetSum(nums,target-nums[index], index+1,dp,offset);
+        int sub=targetSum(nums,target+nums[index],index+1,dp,offset);
+        dp[index][target+offset]=add+sub;
+        return dp[index][target+offset];
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return targetSum(nums,target,0);
+        int sum=0;
+        for(int i=0; i<nums.length; i++){ 
+            sum+=nums[i];
+        }
+        int dp[][]= new int[nums.length][2*sum+1];
+        for(int i=0; i<nums.length; i++){ 
+            Arrays.fill(dp[i],-1);
+        }
+            if (Math.abs(target) > sum) {
+              return 0;
+           }
+        return targetSum(nums,target,0,dp,sum);
     }
 }
