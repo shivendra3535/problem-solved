797a0bff9c05f0057a5ab6c9ae7cdd15edd57a6c
Shivendra Rajat Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-09-03 08:01:04 +0530

    Time: 16 ms (46.86%), Space: 46.8 MB (27.06%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -13,18 +13,36 @@ class Solution {
         dp[index][target+offset]=add+sub;
         return dp[index][target+offset];
     }
+    public int tabulation(int[] nums, int target, int offset){
+        int dp[][]= new int[nums.length+1][2*offset+1];
+        dp[nums.length][offset]=1;
+        for(int i=nums.length-1; i>=0; i--){
+          for(int tgt=-offset; tgt<=offset; tgt++){
+            int add=0;
+            int sub=0;
+            if(tgt-nums[i]>=-offset && tgt-nums[i]<=offset){
+              add=dp[i+1][tgt-nums[i]+offset];}
+
+            if(tgt+nums[i]>=-offset && tgt+nums[i]<=offset){
+              sub=dp[i+1][tgt+nums[i]+offset];}
+            dp[i][tgt+offset]=add+sub;
+          }  
+        }
+        return dp[0][target+offset];
+    }
     public int findTargetSumWays(int[] nums, int target) {
         int sum=0;
         for(int i=0; i<nums.length; i++){ 
             sum+=nums[i];
         }
-        int dp[][]= new int[nums.length][2*sum+1];
+        /*int dp[][]= new int[nums.length][2*sum+1];
         for(int i=0; i<nums.length; i++){ 
             Arrays.fill(dp[i],-1);
+        }*/
+        if (Math.abs(target) > sum) {
+            return 0;
         }
-            if (Math.abs(target) > sum) {
-              return 0;
-           }
-        return targetSum(nums,target,0,dp,sum);
+        //return targetSum(nums,target,0,dp,sum);
+        return tabulation(nums,target,sum);
     }
 }
