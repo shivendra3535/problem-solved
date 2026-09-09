be65c51f09dacf600b4395598beb62fd65424565
Shivendra Rajat Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-09-09 16:35:27 +0530

    Time: 21 ms (54.54%), Space: 46.6 MB (94.53%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -28,12 +28,31 @@ class Solution {
         }
         return dp[text1.length()][text2.length()];
     }
+    public int spaceOpt(String text1, String text2){
+        int prevDp[]= new int[text2.length()+1];
+        for(int i=1; i<text1.length()+1; i++){
+            int tempDp[]=new int[text2.length()+1];
+            for(int j=1; j<text2.length()+1; j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    tempDp[j]=1+prevDp[j-1];
+                }
+                else{
+                    int f1=prevDp[j];
+                    int f2=tempDp[j-1];
+                    tempDp[j]=Math.max(f1,f2);
+                }
+            }
+            prevDp=tempDp;
+        }
+        return prevDp[text2.length()];
+    }
     public int longestCommonSubsequence(String text1, String text2) {
         /*int dp[][]= new int[text1.length()][text2.length()];
         for(int i=0; i<text1.length(); i++){
             Arrays.fill(dp[i],-1);
         }
         return recur(text1,text2,text1.length()-1,text2.length()-1,dp);*/
-        return tabulation(text1,text2);
+        //return tabulation(text1,text2);
+        return spaceOpt(text1,text2);
     }
 }
