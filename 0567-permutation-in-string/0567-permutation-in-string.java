98f9df8937ae031477d97aa8741189a6387659dc
Shivendra Rajat Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-09-12 13:08:59 +0530

    Time: 8 ms (49.59%), Space: 44.4 MB (31.15%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -3,8 +3,8 @@ class Solution {
         int l1=s1.length();
         int l2=s2.length();
         if(l1>l2) return false;
-        int freq1[]=new int[26];
-        int freq2[]=new int[26];
+        int freq1[]= new int[26];
+        int freq2[]= new int[26];
         for(char c: s1.toCharArray()){
             freq1[c-'a']++;
         }
@@ -12,24 +12,22 @@ class Solution {
         for(int i=0; i<26; i++){
             if(freq1[i]>0) req++;
         }
-        int left=0;
+        int cur=0;
         int right=0;
-        int curr=0;
+        int left=0;
         while(right<l1){
-            int a=s2.charAt(right)-'a';
-            freq2[a]++;
-            if(freq1[a]==freq2[a]) curr++;
+            freq2[s2.charAt(right)-'a']++;
+            if(freq1[s2.charAt(right)-'a']==freq2[s2.charAt(right)-'a']) cur++;
             right++;
         }
-        if(curr==req) return true;
+        if(cur==req) return true;
         while(right<l2){
-            int a=s2.charAt(right)-'a';
-            freq2[a]++;
-            if(freq1[a]==freq2[a]) curr++;
-            int b=s2.charAt(left)-'a';
-            if(freq1[b]==freq2[b]) curr--;
-            freq2[b]--;
-            if(curr==req) return true;
+            freq2[s2.charAt(right)-'a']++;
+            if(freq1[s2.charAt(right)-'a']==freq2[s2.charAt(right)-'a']) cur++;
+        
+            if(freq1[s2.charAt(left)-'a']==freq2[s2.charAt(left)-'a']) cur--;
+            freq2[s2.charAt(left)-'a']--;
+            if(cur==req) return true;
             right++;
             left++;
         }
