ee4729ee17fb1707584a1b702ff1008c5053a5de
Shivendra Rajat Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-09-17 17:20:17 +0530

    Time: 53 ms (24.96%), Space: 69.6 MB (5.4%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -20,8 +20,39 @@ class Solution {
 
         return dp[i][j]=false;
     }
+
+    public boolean tabulation(String s, String p){
+        int n=s.length();
+        int m=p.length();
+        boolean dp[][]= new boolean[n+1][m+1];
+        dp[0][0]=true;
+        for(int i=1; i<=n; i++) dp[i][0]=false;
+        for(int j=1; j<=m; j++){
+            if(p.charAt(j-1)=='*'){
+                dp[0][j]=dp[0][j-1];
+            }
+            else{
+                dp[0][j]=false;
+            }
+        }
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else if(p.charAt(j-1)=='*'){
+                    dp[i][j]= dp[i][j-1] || dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=false;
+                }
+            }
+        }
+        return dp[n][m];
+    }
     public boolean isMatch(String s, String p) {
         Boolean dp[][]= new Boolean[s.length()][p.length()];
-        return wild(s,p,s.length()-1,p.length()-1,dp);
+        //return wild(s,p,s.length()-1,p.length()-1,dp);
+        return tabulation(s,p);
     }
 }
