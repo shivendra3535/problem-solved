46830037d777245babb267f87227d01714a1ba2d
Shivendra Rajat Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-09-25 17:54:38 +0530

    Time: 41 ms (15.2%), Space: 161.1 MB (5.02%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,23 +1,21 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> dq= new LinkedList<>();
         List<Integer> res= new ArrayList<>();
-        int n=nums.length;
-        for(int i=0; i<n; i++){
-            while(!dq.isEmpty() && dq.peekFirst()<=i-k){
-                dq.pollFirst();
-            }
-            while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]){
-                dq.pollLast();
-            }
-            dq.offerLast(i);
+        Deque<Integer> dq= new LinkedList<>();
+        for(int i=0; i<nums.length; i++){
+            if(!dq.isEmpty() && dq.peekFirst()<=i-k) dq.pollFirst();
+
+            while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]) dq.pollLast();
+            dq.addLast(i);
+
             if(i>=k-1){
-                res.add(dq.peekFirst());
+                res.add(nums[dq.peekFirst()]);
             }
         }
-        int ans[]= new int[res.size()];
+
+        int []ans= new int[res.size()];
         for(int i=0; i<res.size(); i++){
-            ans[i]=nums[res.get(i)];
+            ans[i]=res.get(i);
         }
         return ans;
     }
