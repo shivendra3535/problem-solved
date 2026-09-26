96a048aac50947426dcb0e00907e6727a231d4f1
Shivendra Rajat Pandey <146531194+shivendra3535@users.noreply.github.com>
2026-09-26 19:12:45 +0530

    Time: 207 ms (20.17%), Space: 280 MB (5.71%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -7,11 +7,27 @@ class Solution {
         if(prevId==nums.length || nums[index]<nums[prevId]) take=1+recur(nums,index-1,index,dp);
         return dp[index][prevId]= Math.max(notTake,take);
     }
+    public int tabulation(int nums[]){
+        int n=nums.length;
+        int dp[][]= new int[n+1][n+1];
+        for(int index=1; index<=n; index++){
+            for(int prev=0; prev<=n; prev++){
+                int notTake=dp[index-1][prev];
+                int take=0;
+                if(prev==n || nums[index-1]<nums[prev]){
+                    take=1+dp[index-1][index-1];
+                }
+                dp[index][prev]=Math.max(take,notTake);
+            }
+        }
+        return dp[n][n];
+    }
     public int lengthOfLIS(int[] nums) {
         int dp[][]=new int[nums.length][nums.length+1];
         for(int i=0; i<nums.length; i++){
             Arrays.fill(dp[i],-1);
         }
-        return recur(nums,nums.length-1,nums.length,dp);
+        //return recur(nums,nums.length-1,nums.length,dp);
+        return tabulation(nums);
     }
 }
